098c1bd3bb5ce99806594e2d353fffcbebf46be3
Lanh Nguyen <lanhnguyen@kms-technology.com>
2016-05-25 20:15:22 +0700

    bookbean

--- src/main/java/com/lib/ManageBean/BooksBean.java
+++ src/main/java/com/lib/ManageBean/BooksBean.java
@@ -2,21 +2,26 @@ package com.lib.managebean;
 
 import com.lib.domain.Book;
 import com.lib.domain.Borrow;
+import com.lib.domain.Copy;
 import com.lib.domain.Location;
 import com.lib.modelview.SearchBookCriteria;
 import com.lib.service.BookService;
 import com.lib.service.BorrowService;
 import com.lib.service.CopyService;
+
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 import com.lib.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by TR on 3/13/2016.
@@ -24,7 +29,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ManagedBean
-public class BooksBean {
+@ViewScoped
+public class BooksBean implements Serializable {
 
     @Autowired
     private BookService bookService;
@@ -41,16 +47,39 @@ public class BooksBean {
     @Autowired
     LocationService locationService;
 
+    private Book selectedBook;
+
+    private Copy copy = new Copy();
+
     private List<Book> filteredBooks;
 
     private List<Location> locations = new ArrayList<>();
+    private Map<Integer, String> mapLocation = new HashMap<>();
+
+
+    private List<Integer> listLocationId = new ArrayList<>();
+
+    private int selectedLocationId;
 
 
     @PostConstruct
     public void init() {
         allBooks = bookService.findAll();
         locations = locationService.getAllLocation();
-       // searchBookCriteria.setName("b");
+
+        mapLocation = new HashMap<>();
+        listLocationId = new ArrayList<>();
+        for (Location location : locations) {
+            mapLocation.put(location.getLId(), location.getLName());
+            listLocationId.add(location.getLId());
+        }
+
+        selectedLocationId = listLocationId.size() > 0? listLocationId.get(0) : 0;
+    }
+
+    public void bookRowOnClick(Book book){
+
+        this.selectedBook = book;
     }
     private Book newBook = new Book();
 
@@ -100,10 +129,10 @@ public class BooksBean {
     }
 
     public void addNewBook() {
-        System.out.println("add");
-        System.out.println(bookService.addNewBook(newBook));
+        if (!StringUtils.isEmpty(newBook.getBName())) {
+            bookService.addNewBook(newBook);
+        }
         searchBook();
-        System.out.println(allBooks.size());
     }
 
     public String borrowBook(Book book) {
@@ -115,6 +144,15 @@ public class BooksBean {
     public int createBorrow(Borrow borrow){
         return borrowService.createBorrow(borrow);
 
+    }
+
+    public int addNewCopy(){
+        if (copy.getCNumber() <=0 ){
+            return 0;
+        }
+        copy.setlId(selectedLocationId);
+        copy.setbId(selectedBook.getBId());
+        return copyService.addNewCopy(copy);
     }
 
     public Book getNewBook() {
@@ -143,5 +181,45 @@ public class BooksBean {
 
     public void setFilteredBooks(List<Book> filteredBooks) {
         this.filteredBooks = filteredBooks;
+    }
+
+    public String getLocationName(int id) {
+        return mapLocation.get(id);
+    }
+
+    public Book getSelectedBook() {
+        return selectedBook;
+    }
+
+    public void setSelectedBook(Book selectedBook) {
+        this.selectedBook = selectedBook;
+    }
+
+    public Copy getCopy() {
+        return copy;
+    }
+
+    public void setCopy(Copy copy) {
+        this.copy = copy;
+    }
+
+    public int getSelectedLocationId() {
+        return selectedLocationId;
+    }
+
+    public void setSelectedLocationId(int selectedLocationId) {
+        this.selectedLocationId = selectedLocationId;
+    }
+
+    public void setNewBook(Book newBook) {
+        this.newBook = newBook;
+    }
+
+    public List<Integer> getListLocationId() {
+        return listLocationId;
+    }
+
+    public void setListLocationId(List<Integer> listLocationId) {
+        this.listLocationId = listLocationId;
     }
 }
