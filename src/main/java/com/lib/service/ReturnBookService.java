613c15abe24940d95424f2935b008591df225510
Lanh Nguyen <lanhnguyen@kms-technology.com>
2016-05-25 13:42:00 +0700

    "add book"

--- src/main/java/com/lib/service/ReturnBookService.java
+++ src/main/java/com/lib/service/ReturnBookService.java
@@ -3,10 +3,12 @@ package com.lib.service;
 import com.lib.dao.ReturnBookDao;
 import com.lib.domain.ReturnBook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by TR on 5/25/2016.
  */
+@Service
 public class ReturnBookService {
     @Autowired
     ReturnBookDao returnBookDao;
